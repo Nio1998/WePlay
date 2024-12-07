201d6394b1e6b19a238077527836bc1b8ff2cfcf
andr3w003 <164201820+andr3w003@users.noreply.github.com>
2024-12-07 14:24:47 +0100

    edit_PrenotazioneBean invertite posizioni attributi per combaciare il db

--- Application/src/main/java/model/prenotazione/PrenotazioneBean.java
+++ Application/src/main/java/model/prenotazione/PrenotazioneBean.java
@@ -8,46 +8,40 @@ public class PrenotazioneBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	private int eventoID;
 	private String utenteUsername;
-	private int posizioneInCoda;
+	private int eventoID;
 	private String stato;
+	private int posizioneInCoda;
 	
 	public PrenotazioneBean() {}
 	
-	public PrenotazioneBean(int eventoID, String utenteUsername, int posizioneInCoda, String stato) {
+	public PrenotazioneBean(String utenteUsername, int eventoID, String stato, int posizioneInCoda) {
 		super();
-		this.eventoID = eventoID;
 		this.utenteUsername = utenteUsername;
-		this.posizioneInCoda = posizioneInCoda;
+		this.eventoID = eventoID;
 		this.stato = stato;
+		this.posizioneInCoda = posizioneInCoda;
 	}
 	
 	public PrenotazioneBean(ResultSet rs) throws SQLException {
 		super();
-		this.eventoID = rs.getInt("ID_evento");
 		this.utenteUsername = rs.getString("username_utente");
-		this.posizioneInCoda = rs.getInt("posizione_in_coda");
+		this.eventoID = rs.getInt("ID_evento");
 		this.stato = rs.getString("stato");
+		this.posizioneInCoda = rs.getInt("posizione_in_coda");
 	}
 	
-	public int getEventoID() {
-		return eventoID;
-	}
-	public void setEventoID(int eventoID) {
-		this.eventoID = eventoID;
-	}
 	public String getUtenteUsername() {
 		return utenteUsername;
 	}
 	public void setUtenteUsername(String utenteUsername) {
 		this.utenteUsername = utenteUsername;
 	}
-	public int getPosizioneInCoda() {
-		return posizioneInCoda;
+	public int getEventoID() {
+		return eventoID;
 	}
-	public void setPosizioneInCoda(int posizioneInCoda) {
-		this.posizioneInCoda = posizioneInCoda;
+	public void setEventoID(int eventoID) {
+		this.eventoID = eventoID;
 	}
 	public String getStato() {
 		return stato;
@@ -55,11 +49,17 @@ public class PrenotazioneBean implements Serializable {
 	public void setStato(String stato) {
 		this.stato = stato;
 	}
+	public int getPosizioneInCoda() {
+		return posizioneInCoda;
+	}
+	public void setPosizioneInCoda(int posizioneInCoda) {
+		this.posizioneInCoda = posizioneInCoda;
+	}
 
 	@Override
 	public String toString() {
-		return "PrenotazioneBean [eventoID=" + eventoID + ", utenteUsername=" + utenteUsername + ", posizioneInCoda="
-				+ posizioneInCoda + ", stato=" + stato + "]";
+		return "PrenotazioneBean [utenteUsername=" + utenteUsername + ", eventoID=" + eventoID + ", stato=" + stato
+				+ ", posizioneInCoda=" + posizioneInCoda + "]";
 	}
 	
 }
