3f6a4a48bf9f00ebd224c0c905ad56bba28b7da3
pummarolaalragu <dragone640@hotmail.it>
2024-12-08 21:46:44 +0100

    Aggiunto ValutazioneBean e ValutazioneDAO nella cartella Valutazione

---
+++ Application/src/main/java/model/Valutazione/ValutazioneBean.java
@@ -0,0 +1,68 @@
+package model.Valutazione;
+
+public class ValutazioneBean {
+    private static int idCounter = 1; // Variabile statica per calcolare l'ID automaticamente
+    private int id; // ID univoco per la valutazione
+    private String esito; // Valori ammessi: "-1" (negativa), "0" (neutra), "1" (positiva)
+    private String utenteValutato; // Utente che riceve la valutazione
+    private String utenteValutante; // Utente che effettua la valutazione
+    private int idEvento; // ID dell'evento associato
+
+    // Costruttore
+    public ValutazioneBean(String esito, String utenteValutato, String utenteValutante, int idEvento) {
+        this.id = idCounter++;
+        this.esito = esito;
+        this.utenteValutato = utenteValutato;
+        this.utenteValutante = utenteValutante;
+        this.idEvento = idEvento;
+    }
+
+    // Getter e Setter
+    public int getId() {
+        return id;
+    }
+
+    public String getEsito() {
+        return esito;
+    }
+
+    public void setEsito(String esito) {
+        this.esito = esito;
+    }
+
+    public String getUtenteValutato() {
+        return utenteValutato;
+    }
+
+    public void setUtenteValutato(String utenteValutato) {
+        this.utenteValutato = utenteValutato;
+    }
+
+    public String getUtenteValutante() {
+        return utenteValutante;
+    }
+
+    public void setUtenteValutante(String utenteValutante) {
+        this.utenteValutante = utenteValutante;
+    }
+
+    public int getIdEvento() {
+        return idEvento;
+    }
+
+    public void setIdEvento(int idEvento) {
+        this.idEvento = idEvento;
+    }
+
+    // Override del metodo toString
+    @Override
+    public String toString() {
+        return "Valutazione [" +
+               "id=" + id +
+               ", esito='" + esito + '\'' +
+               ", utenteValutato='" + utenteValutato + '\'' +
+               ", utenteValutante='" + utenteValutante + '\'' +
+               ", idEvento=" + idEvento +
+               ']';
+    }
+}
