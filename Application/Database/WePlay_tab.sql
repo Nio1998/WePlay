10310078269bbfc38575d2e7569ddf6feb18f761
Kevins2205 <kevin220503@gmail.com>
2024-12-12 19:02:05 +0100

    edit InserimentoDati/CreazioneTabelle

--- Application/Database/WePlay_tab.sql
+++ Application/Database/WePlay_tab.sql
@@ -70,8 +70,8 @@ DROP TABLE IF EXISTS valutazione;
 CREATE TABLE valutazione (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     esito INT NOT NULL, -- negativa, neutra, positiva
-    utente_segnalato VARCHAR(50),
-    utente_segnalante VARCHAR(50),
+    utente_valutato VARCHAR(50),
+    utente_valutante VARCHAR(50),
     ID_evento INT,
     FOREIGN KEY (utente_segnalato) REFERENCES Utente(username),
     FOREIGN KEY (utente_segnalante) REFERENCES Utente(username),
