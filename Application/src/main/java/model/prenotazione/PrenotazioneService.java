b5c22a3135938520ec325045b86e6b03c60a057a
Kevins2205 <kevin220503@gmail.com>
2024-12-12 12:48:19 +0100

    edit_Prenotazione Service && PrenotazioneDao

--- Application/src/main/java/model/prenotazione/PrenotazioneService.java
+++ Application/src/main/java/model/prenotazione/PrenotazioneService.java
@@ -1,6 +1,10 @@
 package model.prenotazione;
 
 import java.sql.SQLException;
+import java.util.Collection;
+
+
+import model.utente.UtenteBean;
 
 public class PrenotazioneService {
 
@@ -10,7 +14,19 @@ public class PrenotazioneService {
         this.prenotazioneDAO = new PrenotazioneDAO();
     }
 
- 
+    
+    public Collection<UtenteBean> calcola_partecipanti(int evento_id){
+    	try {
+			return prenotazioneDAO.getAllWhereEvento(evento_id);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	return null;
+    }
+    
+    
+    
     public boolean cancella_prenotazione(String usernameUtente, int eventoID) {
         if (usernameUtente == null || usernameUtente.isEmpty()) {
             throw new IllegalArgumentException("Il nome utente non può essere nullo o vuoto.");
